4cd4536d834d7f20913750658d9783a513e4a7cb
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-14 15:00:38 +0900

    Create firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAXzCD9kyGdxMFr_GoLUiH8_XCsS_mi52Y",
+  authDomain: "ec-app-89724.firebaseapp.com",
+  projectId: "ec-app-89724",
+  storageBucket: "ec-app-89724.appspot.com",
+  messagingSenderId: "106762495572",
+  appId: "1:106762495572:web:87ca879def6d0235acefc1",
+  measurementId: "G-5FP4ZB0Z89"
+};
