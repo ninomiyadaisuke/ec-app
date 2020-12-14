9247611052af488751684c3672cbd75b0bb49979
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-14 16:48:39 +0900

    create SignUp

--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -1,5 +1,6 @@
 import { signInAction } from "./actions"
 import { push } from "connected-react-router"
+import { auth, FirebaseTimeStamp, db } from "../../firebase/index"
 
 export const signIn = () => {
   return async (dispatch, getState) => {
@@ -20,5 +21,43 @@ export const signIn = () => {
       }))
       dispatch(push("/"))
     }
+  }
+}
+
+export const signUp = (userName, email, password, confirmPassword) => {
+  return async (dispatch) => {
+    if (userName === "" || email === "" || password === "" || confirmPassword === "") {
+      alert("必須項目が未入力です")
+      return false
+    }
+
+    if (password !== confirmPassword) {
+      alert("パスワードが一致してません。もう一度お試しください")
+      return false
+    }
+
+    return auth.createUserWithEmailAndPassword(email, password)
+      .then(result => {
+        const user = result.user
+
+        if (user) {
+          const uid = user.uid
+          const timestamp = FirebaseTimeStamp.now()
+
+          const userInitialData = {
+            created_at: timestamp,
+            email: email,
+            role: "customer",
+            uid: uid,
+            update_at: timestamp,
+            userName: userName
+          }
+          db.collection("users").doc(uid).set(userInitialData)
+            .then(() => {
+              dispatch(push("/"))
+            })
+        }
+      
+      })
   }
 }
