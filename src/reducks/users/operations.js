738ca0a2cbcead07484f769d3a84985f44e477e6
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-15 09:04:10 +0900

    Create an authentication function

--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -2,6 +2,32 @@ import { signInAction } from "./actions"
 import { push } from "connected-react-router"
 import { auth, FirebaseTimeStamp, db } from "../../firebase/index"
 
+export const listenAuthState = () => {
+  return async (dispatch) => {
+    return auth.onAuthStateChanged(user => {
+      if (user) {
+          const uid = user.uid
+
+          db.collection("users").doc(uid).get()
+            .then(snapshot => {
+              const data = snapshot.data()
+
+              dispatch(signInAction({
+                isSignedIn: true,
+                role: data.role,
+                uid: uid,
+                userName: data.userName
+              }))
+
+              dispatch(push("/"))
+            })        
+      } else {
+        dispatch(push("/signin"))
+      }
+    })
+  }
+}
+
 export const signIn = (email, password) => {
   return async (dispatch) => {
     if (email === "" || password === "") {
@@ -27,7 +53,6 @@ export const signIn = (email, password) => {
               }))
 
               dispatch(push("/"))
-              
             })
         }
         })
