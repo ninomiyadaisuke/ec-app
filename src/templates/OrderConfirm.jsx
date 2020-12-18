e612ad5bb3b94a9ae0c6879af45a7dc65ded1cfd
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-18 16:34:09 +0900

    create TextDetail commponent

--- src/templates/OrderConfirm.jsx
+++ src/templates/OrderConfirm.jsx
@@ -1,11 +1,11 @@
 import React, { useCallback, useMemo } from 'react'
 import { useDispatch, useSelector } from "react-redux"
-import { getProductInCart } from "../reducks/users/selectors"
+import { getProductsInCart }  from "../reducks/users/selectors"
 import { makeStyles } from "@material-ui/styles"
 import { CartListItem } from "../components/Products"
 import List from "@material-ui/core/List"
 import Divider from "@material-ui/core/Divider"
-import { PrimaryButton } from "../components/UIkit"
+import { PrimaryButton, TextDetail } from "../components/UIkit"
 
 const useStyles = makeStyles((theme) => ({
   detailBox: {
@@ -33,7 +33,15 @@ const OrderConfirm = () => {
   const classes = useStyles()
   const dispatch = useDispatch()
   const selector = useSelector((state) => state)
-  const productsInCart = getProductInCart(selector)
+  const productsInCart = getProductsInCart(selector)
+
+  const subtotal = useMemo(() => {
+    return productsInCart.reduce((sum, product) => sum += product.price, 0)
+  }, [productsInCart])
+
+  const shippingFee = (subtotal >= 10000) ? 0 : 210
+  const tax = subtotal * 0.1
+  const total = subtotal + shippingFee + tax
 
   return (
     <section className="c-section-wrapin">
@@ -47,7 +55,11 @@ const OrderConfirm = () => {
           </List>
         </div>
         <div className={classes.orderBox}>
-          
+          <TextDetail label={"商品合計"} value={"¥" + subtotal.toLocaleString()} />
+          <TextDetail label={"消費税"} value={"¥" + tax.toLocaleString()} />
+          <TextDetail label={"送料"} value={"¥" + shippingFee.toLocaleString()} />
+          <Divider/>
+          <TextDetail label={"合計（税込）"} value={"¥" + total.toLocaleString()} />
         </div>
       </div>
     </section>
--- src/templates/index.js
+++ src/templates/index.js
@@ -7,4 +7,5 @@ export { default as ProductEdit } from "./ProductEdit"
 export { default as ProductList } from "./ProductList"
 export { default as ProductDetail } from "./ProductDetail"
 export { default as CartList } from "./CartList"
+export { default as OrderConfirm } from "./OrderConfirm"
 
