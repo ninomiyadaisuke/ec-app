684801cf82d0e396881443d60a7dfe5333f60f5f
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-21 13:04:26 +0900

    create OrderHistoryItem

--- src/templates/OrderHistory.jsx
+++ src/templates/OrderHistory.jsx
@@ -4,6 +4,7 @@ import List from "@material-ui/core/List"
 import { makeStyles } from "@material-ui/styles"
 import { getOrdersHistory } from '../reducks/users/selectors'
 import { fetchOrdersHistory } from '../reducks/users/operations'
+import { OrderHistoryItem } from "../components/Products"
 
 const useStyles = makeStyles((theme) => ({
   orderList: {
@@ -34,7 +35,9 @@ const OrderHistory = () => {
   return (
     <section className="c-section-wrapin">
       <List className={classes.orderList}>
-
+        {orders.length > 0 && (
+          orders.map(order => <OrderHistoryItem order={order} key={order.id}/>)
+        )}
       </List>
 
     </section>
